df3fbb2f9df01b47f58c34bd58041f4a92744cc9
sidxdev <sidsriv93@gmail.com>
2026-02-14 18:15:54 +0530

    fix for alpine

--- pocketbase/entrypoint.sh
+++ pocketbase/entrypoint.sh
@@ -2,26 +2,18 @@
 set -e
 
 # Check required environment variables
-ENV_NOT_SET=false
-# Function to check required environment variables
 check_env_var() {
   local var_name=$1
-  if [ -z "${!var_name}" ]; then
+  eval "var_value=\$$var_name"
+  if [ -z "$var_value" ]; then
     echo "Error: Environment Variable $var_name is not set. Exiting."
-    ENV_NOT_SET=true
+    exit 1
   fi
 }
 
-# List of required environment variables
-REQUIRED_ENV_VARS=("ADMIN_EMAIL" "ADMIN_PASSWORD")
-
-for var in "${REQUIRED_ENV_VARS[@]}"; do
-  check_env_var "$var"
-done
-
-if [ "$ENV_NOT_SET" = true ]; then
-  exit 1
-fi
+# Check required variables
+check_env_var "ADMIN_EMAIL"
+check_env_var "ADMIN_PASSWORD"
 
 # Create Admin Superuser
 /usr/local/bin/pocketbase superuser upsert "$ADMIN_EMAIL" "$ADMIN_PASSWORD" --dir=/pb_data
